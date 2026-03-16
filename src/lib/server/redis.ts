10af6a92553c95ebe68c041c6f0fbc8c1d0c3d9d
FredStoney <fredstoney22@gmail.com>
2026-03-16 12:15:50 -0400

    redis file

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,8 +1,9 @@
 import { Redis } from '@upstash/redis';
-import { KV_REST_API_URL, KV_REST_API_TOKEN } from '$env/static/private';
+
+const { KV_REST_API_URL = '', KV_REST_API_TOKEN = '' } = process.env;
 
 export const redis = new Redis({
-	url: KV_REST_API_URL || '',
-	token: KV_REST_API_TOKEN || ''
+	url: KV_REST_API_URL,
+	token: KV_REST_API_TOKEN
 });
 
